a61cbf25e5f1fa15479f3b0c88aa4aae39e4b9ac
Brent Westbrook <bwestbro@stedwards.edu>
2018-02-23 20:01:23 -0600

    Fixed to_normal_coord

--- improvedStatsInside.py
+++ improvedStatsInside.py
@@ -3,22 +3,22 @@ def innertube(z_sections):
 	"""Takes the lampstrj file produced by the CHILL Algorithm and calculates the percent of each type of
 		water, printing these to files for each type of water for plotting"""
 
-#	filename = raw_input("Enter the file to be read: ")
-	filename = "vmd.lammpstrj"
-	infile = open(filename, 'r')
+#	file_name = raw_input("Enter the file to be read: ")
+	file_name = "vmd.lammpstrj"
+	infile = open(file_name, 'r')
 	print(infile, "is being read")
 
-	outname1 = filename + ".hex.perc"
-	outname2 = filename + ".cub.perc"
-	outname3 = filename + ".ifcl.perc"
-	outname4 = filename + ".ldl.perc"
-	outname5 = filename + ".hdl.perc" 
+	out_name1 = file_name + ".hex.perc"
+	out_name2 = file_name + ".cub.perc"
+	out_name3 = file_name + ".ifcl.perc"
+	out_name4 = file_name + ".ldl.perc"
+	out_name5 = file_name + ".hdl.perc" 
 
-	out1 = open(outname1, 'w')
-	out2 = open(outname2, 'w')
-	out3 = open(outname3, 'w')
-	out4 = open(outname4, 'w')
-	out5 = open(outname5, 'w')
+	out1 = open(out_name1, 'w')
+	out2 = open(out_name2, 'w')
+	out3 = open(out_name3, 'w')
+	out4 = open(out_name4, 'w')
+	out5 = open(out_name5, 'w')
 
 	out1.write("#Frame Proportion Hexagonal Ice \n")
 	out2.write("#Frame Cubic Ice\n")
@@ -33,21 +33,31 @@ def innertube(z_sections):
 	total_counter = 0
 	total_atoms = 0
 
+	x_low = 0
+	x_high = 0
+	y_low = 0
+	y_high = 0
+	z_low = 0
+	z_high = 0
+
 	for line in infile:
 		if line == "ITEM: TIMESTEP\n":
-			line = skip(line, infile, 2)
-			split = infile.next().split()
+			line = skip(line, infile, 3)
+			split = line.split()
 			total_atoms = split[0]
-			split = infile.next().split()
+			line = skip(line, infile, 2)
+			split = line.split()
 			x_low = split[0]
 			x_high = split[1]
-			split = infile.next().split()
+			line = skip(line, infile, 1)
+			split = line.split()
 			y_low = split[0]
 			y_high = split[1]
-			split = infile.next().split()
+			line = skip(line, infile, 1)
+			split = line.split()
 			z_low = split[0]
 			z_high = split[1]
-			line = skip(line, infile, 3)
+			line = skip(line, infile, 2)
 			frame_counter += 1
 			atoms = True
 			
@@ -57,8 +67,8 @@ def innertube(z_sections):
 			if int(split[1]) < 6:
 				types[int(split[1])-1] += 1
 				atom_counter += 1
+
 		if total_counter == int(total_atoms):
-			print types, frame_counter, atom_counter, total_counter
 			atom_counter = 0
 			total_counter = 0
 			types = [0 for i in range(5)]
@@ -70,9 +80,12 @@ def skip(iterator, file, num):
 		iterator = file.next()
 	return iterator
 
-def toNormalCoord(low, high, relativeCoord):
-	convertedCoord = (high - low)*relativeCoord + low
-	return convertedCoord
+def to_normal_coord(low, high, relative_coord):
+	low = float(low)
+	high = float(high)
+	relative_coord = float(relative_coord)
+	converted_coord = (high - low)*relative_coord + low
+	return converted_coord
 
 innertube(5)
 
